dba1a14c6603da314cfd5f468ed85e9c7b7bb365
Alisson Breno <alissonbreno36@hotmail.com>
2023-06-21 16:21:52 -0300

    Pausa pra aula

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-hello word
+import { nanoid } from 'nanoid';
+import './style.css';
+
+const passowordBottom = document.querySelector('button');
+const displayPssowordBottom = document.querySelector('h2');
+
+passowordBottom.addEventListener('click', () => {
+    const randomPassoword = nanoid();
+    displayPssowordBottom.innerHTML = randomPassoword;
+});
