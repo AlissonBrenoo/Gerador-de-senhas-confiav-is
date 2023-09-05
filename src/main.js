17246e2c3477a5deb03cea81df4f20f59e28c5c5
Alisson Breno <alissonbreno36@hotmail.com>
2023-09-05 15:50:49 -0300

    Configurando lint

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+import copy from 'clipboard-copy';
 
 const passowordBottom = document.querySelector('button');
 const displayPssowordBottom = document.querySelector('h2');
+console.log('hello world');
 
 passowordBottom.addEventListener('click', () => {
-    const randomPassoword = nanoid();
-    displayPssowordBottom.innerHTML = randomPassoword;
+  const randomPassoword = nanoid();
+  displayPssowordBottom.innerHTML = randomPassoword;
+});
+
+displayPssowordBottom.addEventListener('click', () => {
+  copy(displayPssowordBottom.innerText);
 });
